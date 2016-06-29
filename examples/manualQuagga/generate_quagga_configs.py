b029c26ab2889811a4d6ba52babb8ee3431e2d21
david <tranlam@wisc.edu>
2016-06-28 19:00:25 -0700

    Added host_name to host proto, generatequaggaconfigs now can handle a topology (i.e., it can handle neighbors in neighboreconfig)

--- examples/manualQuagga/generate_quagga_configs.py
+++ examples/manualQuagga/generate_quagga_configs.py
@@ -22,22 +22,75 @@ class GenerateQuaggaConfigs():
 
     def CreateBgpdConfigs(self):
         """Creates a list of bgp configs from the quagga_bgpd_template.j2 using the
-protobuf Host and Topology message type. Topology informs the neighbor quagga
-configuration lines and the host informs the router-id and router bgp lines
+        protobuf Host and Topology message type. Topology informs the neighbor quagga
+        configuration lines and the host informs the router-id and router bgp lines
         It ignores HT_LOOKUP service type host
 
-        Returns: a list of configurations to be written to files
+        Returns: a dict of hostname to configurations to be written to files
 
         """
+
+        hostname_to_host_dict = {}
+        for host_proto in self.protobuf_Hosts_:
+            host_name = host_proto.host_name
+            hostname_to_host_dict[host_name] = host_proto
+
+
         template = 'quagga_template.j2'
         bgpd_template = self.jinja2_env_.get_template(template)
-        config_list = []
+        config_dict = {}
+        # for each host proto in hosts, it should generate configs only if it
+        # not hosttype lookupservice
         for host_proto in self.protobuf_Hosts_:
             if(host_proto.host_type == QuaggaTopo_pb2.HostType.Value('HT_LOOKUPSERVICE')):
                 continue
             template_variable_dict = {}
             template_variable_dict['asn'] = host_proto.as_num
             template_variable_dict['ip'] = host_proto.ip
-            config_list.append(bgpd_template.render(template_variable_dict))
-        return config_list
+            self.HandleTopology(template_variable_dict, hostname_to_host_dict, host_proto)
+            config_dict[host_proto.host_name]= bgpd_template.render(template_variable_dict)
+        # print "CONIFG DICT after HANDLE TOPO ", config_dict['a1']
+        return config_dict
+
+    def HandleTopology(self, template_variable_dict, hostname_to_host_dict, host_proto):
+        """Mutates the template variable dictionary for a given host based on the topology
+        entry if one exists.
+
+        Arguments:
+           template_variable_dict: The configuration dictionary that will be used
+            to render the configuration. Will be mutated
+           hostname_to_host_dict: lookup table for accessing host_protos of other
+            hosts. Will be used when using the adjacentnodenames as a lookupkey
+           host_proto: the host configuration being generated. Used to figure
+           out what primary_node_name should be keyed on in the adjacency list
+        """
+        if self.protobuf_Topologys_ == None:
+            return
+        #find the adjacent nodes to the host being configured
+        #Topologies_[0] because there will only ever be 1 topology
+        adjacent_nodes = []
+        for adjacency_list_entry in self.protobuf_Topologys_[0].adjacency_list_entries:
+            if adjacency_list_entry.primary_node_name == host_proto.host_name:
+                for adjacent_node_name in adjacency_list_entry.adjacent_node_names:
+                    adjacent_nodes.append(adjacent_node_name)
+                break
+        # for each adjacent node, find the corresponding hostproto and add it
+        # to the neighbor list of dictionaries. Add it to the template_variable_dict at
+        # the end
+        neighbors = []
+        # print 'adjacentnodes ', adjacent_nodes
+        for adjacent_node in adjacent_nodes:
+            neighbor_dict = {}
+            adjacent_host_proto = hostname_to_host_dict[adjacent_node]
+            adjacent_host_ip = adjacent_host_proto.ip
+            adjacent_host_asn = adjacent_host_proto.as_num
+            neighbor_dict['ip'] = adjacent_host_ip
+            neighbor_dict['as'] = adjacent_host_asn
+            neighbors.append(neighbor_dict)
+        template_variable_dict['neighbors'] = neighbors
+        # print "CONIFG DICT FROM HANDLE TOPO ", template_variable_dict
+
+
+
+
 
--- examples/manualQuagga/generate_quagga_configs_test.py
+++ examples/manualQuagga/generate_quagga_configs_test.py
@@ -51,6 +51,7 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
 
         kHost1 = """
         host_type : HT_QUAGGA
+        host_name: 'a1'
         ip: '192.168.1.1'
         lo_ip: '10.10.1.1'
         as_num: 100
@@ -61,7 +62,7 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
         self.generate_quagga_configs_ = GenerateQuaggaConfigs(host_list, None, self.jinja2_env_)
 
         result_configs_list = self.generate_quagga_configs_.CreateBgpdConfigs()
-        self.assertEquals(result_configs_list[0], kCorrectOutput)
+        self.assertEquals(result_configs_list['a1'], kCorrectOutput)
 
 
     def testCreateBgpdConfigs_GivenTwoHost_TwoStringConfigReturned(self):
@@ -90,12 +91,14 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
 
         kHost1 = """
         host_type : HT_QUAGGA
+        host_name: 'a1'
         ip: '192.168.1.1'
         lo_ip: '10.10.1.1'
         as_num: 100
         """
         kHost2 = """
         host_type : HT_QUAGGA
+        host_name: 'b1'
         ip: '192.168.1.2'
         lo_ip: '10.10.1.2'
         as_num: 101
@@ -111,8 +114,8 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
         self.generate_quagga_configs_ = GenerateQuaggaConfigs(host_list, None, self.jinja2_env_)
 
         result_configs_list = self.generate_quagga_configs_.CreateBgpdConfigs()
-        self.assertEquals(result_configs_list[0], kCorrectOutputHost1)
-        self.assertEquals(result_configs_list[1], kCorrectOutputHost2)
+        self.assertEquals(result_configs_list['a1'], kCorrectOutputHost1)
+        self.assertEquals(result_configs_list['b1'], kCorrectOutputHost2)
 
 
     def testCreateBgpdConfigs_GivenTwoHostWithOneOfThemBeingATypeLookUpService_OneStringConfigReturned(self):
@@ -139,12 +142,14 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
 
         kHost1 = """
         host_type : HT_QUAGGA
+        host_name: 'a1'
         ip: '192.168.1.1'
         lo_ip: '10.10.1.1'
         as_num: 100
         """
         kHost2 = """
         host_type : HT_LOOKUPSERVICE
+        host_name: 'b1'
         ip: '192.168.1.2'
         lo_ip: '10.10.1.2'
         as_num: 101
@@ -158,8 +163,88 @@ class GenerateQuaggaConfigsTest(unittest.TestCase):
         self.generate_quagga_configs_ = GenerateQuaggaConfigs(host_list, None, self.jinja2_env_)
 
         result_configs_list = self.generate_quagga_configs_.CreateBgpdConfigs()
-        self.assertEquals(result_configs_list[0], kCorrectOutputHost1)
+        self.assertEquals(result_configs_list['a1'], kCorrectOutputHost1)
         self.assertEquals(len(result_configs_list), 1)
+
+
+    def testCreateBgpdConfigs_GivenTwoHostWithTopoConnectingThem_ConfigsCorrect(self):
+        kCorrectOutputHost1 = """
+        ! path logfile for this daemon (BGPD)
+        log file /var/log/quagga/bgpd.log
+
+        ! the password to use for telnet authentication
+        password bgpuser
+
+        ! this routers AS number and BGP ID
+        router bgp 100
+        bgp router-id 192.168.1.1
+
+        neighbor 192.168.1.2 remote-as 101
+
+        ! the network this router will advertise
+        ! network 10.0.1.0/24
+        """
+
+        kCorrectOutputHost2 = """
+        ! path logfile for this daemon (BGPD)
+        log file /var/log/quagga/bgpd.log
+
+        ! the password to use for telnet authentication
+        password bgpuser
+
+        ! this routers AS number and BGP ID
+        router bgp 101
+        bgp router-id 192.168.1.2
+
+        neighbor 192.168.1.1 remote-as 100
+
+        ! the network this router will advertise
+        ! network 10.0.1.0/24
+        """
+
+        kCorrectOutputHost1 = '\n    ! path logfile for this daemon (BGPD)\n    log file /var/log/quagga/bgpd.log\n\n    ! the password to use for telnet authentication\n    password bgpuser\n\n    ! this routers AS number and BGP ID\n    router bgp 100\n    bgp router-id 192.168.1.1\n\n    \n    neighbor 192.168.1.2 remote-as 101\n    \n\n    ! the network this router will advertise\n    ! network 10.0.1.0/24\n    ' 
+        kCorrectOutputHost2 = '\n    ! path logfile for this daemon (BGPD)\n    log file /var/log/quagga/bgpd.log\n\n    ! the password to use for telnet authentication\n    password bgpuser\n\n    ! this routers AS number and BGP ID\n    router bgp 101\n    bgp router-id 192.168.1.2\n\n    \n    neighbor 192.168.1.1 remote-as 100\n    \n\n    ! the network this router will advertise\n    ! network 10.0.1.0/24\n    '
+
+        kHost1 = """
+        host_type : HT_QUAGGA
+        host_name: 'a1'
+        ip: '192.168.1.1'
+        lo_ip: '10.10.1.1'
+        as_num: 100
+        """
+        kHost2 = """
+        host_type : HT_QUAGGA
+        host_name: 'b1'
+        ip: '192.168.1.2'
+        lo_ip: '10.10.1.2'
+        as_num: 101
+        """
+        kTopology = """
+        adjacency_list_entries {
+        primary_node_name: 'a1'
+        adjacent_node_names: 'b1'
+        }
+        adjacency_list_entries {
+        primary_node_name: 'b1'
+        adjacent_node_names: 'a1'
+        }
+        """
+
+        host_list = []
+        tmp_host = QuaggaTopo_pb2.Host()
+        host_list.append(Merge(kHost1, tmp_host))
+        tmp_host = QuaggaTopo_pb2.Host()
+        host_list.append(Merge(kHost2, tmp_host))
+
+        topo_list = []
+        tmp_topology = QuaggaTopo_pb2.Topology()
+        topo_list.append(Merge(kTopology, tmp_topology))
+
+        self.generate_quagga_configs_ = GenerateQuaggaConfigs(host_list, topo_list, self.jinja2_env_)
+
+        result_configs_list = self.generate_quagga_configs_.CreateBgpdConfigs()
+        self.assertEquals(result_configs_list['a1'], kCorrectOutputHost1)
+        self.assertEquals(result_configs_list['b1'], kCorrectOutputHost2)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(GenerateQuaggaConfigsTest)
 runner = unittest.TextTestRunner()
