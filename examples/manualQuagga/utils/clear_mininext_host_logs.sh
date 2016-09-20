efbdebffe259d0ff9494ffa6929b1319a62a44fd
david <tranlam@wisc.edu>
2016-09-20 02:09:57 -0500

    protobuf config is now the paper topo. added more clearing entry to util that cleras logs

--- examples/manualQuagga/utils/clear_mininext_host_logs.sh
+++ examples/manualQuagga/utils/clear_mininext_host_logs.sh
@@ -3,3 +3,6 @@ truncate -s 0 /var/log/mininext/a1/quagga/bgpd.log
 truncate -s 0 /var/log/mininext/b1/quagga/bgpd.log
 truncate -s 0 /var/log/mininext/c1/quagga/bgpd.log
 truncate -s 0 /var/log/mininext/d1/quagga/bgpd.log
+truncate -s 0 /var/log/mininext/e1/quagga/bgpd.log
+truncate -s 0 /var/log/mininext/f1/quagga/bgpd.log
+truncate -s 0 /var/log/mininext/g1/quagga/bgpd.log
